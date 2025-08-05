890d755b658f8dd9b226e21e38a07e29522ebe4f
Vipin Yadav <vipinvyadav9@gmail.com>
2025-08-05 23:53:39 +0530

    update

--- 00-Backend/04-Practice/index.js
+++ 00-Backend/04-Practice/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,7 +9,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.render("index");
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(`./file/${req.params.filename}`, "utf-8",function (err, filedata) {
+    res.render('show') 
+  });
+});
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
