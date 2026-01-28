33f28dd65bd4c1f40e2bb8c239a21ee0248f52e9
Vipin Yadav <vipinvyadav9@gmail.com>
2026-01-28 20:40:47 +0530

    leetcode

--- leetcode/practice-subset.cpp
+++ leetcode/practice-subset.cpp
@@ -1,20 +1,26 @@
 #include "bits/stdc++.h"
 using namespace std;
-void rec(int index, vector<char> &str, string output)
+void rec(int index, vector<char> &str, string output, vector<string> &ans)
 {
     if (index == str.size())
     {
-        cout << output << endl;
+        if(output=="")return;
+        ans.push_back(output);
         return;
     }
-    rec(index + 1, str, output);
-    rec(index + 1, str, output+str[index]);
+
+    rec(index + 1, str, output, ans);
+    rec(index + 1, str, output + str[index], ans);
 }
 int main()
 {
     cout << "Hello World!" << endl;
-    vector<char>str={'a','b','c'};
+    vector<char> str = {'a', 'b', 'c'};
+    vector<string> ans;
 
-
-    rec(0,str,"");
+    rec(0, str, "", ans);
+    for (auto &s : ans)
+    {
+        cout << s << endl;
+    }
 }
